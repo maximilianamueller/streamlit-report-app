895b6424ca4991c5a36a42f15a5790ebadbaee43
maximilianamueller <maximilian.a.mueller@gmail.com>
2025-05-07 13:46:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.sidebar.header("Chart Type")
 plot_type = st.sidebar.radio("Select plot type:", ["Strip Plot", "Violin Plot", "Histogram", "Bar Chart"])
 
 # Title
-st.title("PDF Report Benchmarking")
+st.title("CSRD Report Benchmarking")
 st.subheader(f"Distribution of Pages ({benchmark_label})")
 
 # Focal value
